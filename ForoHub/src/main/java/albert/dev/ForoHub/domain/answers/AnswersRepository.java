124a045700b48e94c2762e8d0cb456b607509062
Albert Dev <125626643+Albertscpv@users.noreply.github.com>
2024-06-27 14:11:12 -0600

    Delete ForoHub directory

--- ForoHub/src/main/java/albert/dev/ForoHub/domain/answers/AnswersRepository.java
+++
@@ -1,8 +0,0 @@
-package albert.dev.ForoHub.domain.answers;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswersRepository extends JpaRepository<Answers, Long> {
-
-
-}
--- ForoHub/src/main/java/albert/dev/ForoHub/domain/answers/UpdateAnswersDTO.java
+++
@@ -1,9 +0,0 @@
-package albert.dev.ForoHub.domain.answers;
-
-public record UpdateAnswersDTO(
-        Long Id,
-        String messageToAnswer,
-        String answer,
-        String creation_date
-) {
-}
